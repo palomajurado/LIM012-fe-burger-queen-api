b9187d804052a387f941594f27473afba0d72a41
Elena <karengvcs@gmail.com>
2020-07-15 00:45:40 -0500

    Create controllers to user, order and product

--- src/controller/__tests__/users.spec.js
+++ src/controller/__tests__/users.spec.js
@@ -1,6 +1,5 @@
-const {
-  getUsers,
-} = require('../../controller/users');
+/* eslint-disable no-unused-vars */
+import { getUsers } from '../users';
 
 describe('getUsers', () => {
   it('should get users collection', (done) => {
---
+++ src/controller/orders.js
@@ -0,0 +1,41 @@
+import Order from '../models/order';
+import Product from '../models/product';
+
+module.exports = {
+  getOrders: async (req, res) => {
+    const orders = await Order.find();
+    res.json(orders);
+  },
+  getOneOrder: async (req, res) => {
+    const orderFound = await Order.findById(req.params.orderId);
+    res.json(orderFound);
+  },
+  createOrder: async (req, res, next) => {
+    const productsArray = await Product.find({ _id: { $in: req.body.products.map((p) => p.productId) } });
+
+    req.body.products = req.body.products.map((item, key) => ({
+      product: {
+        _id: item.productId,
+        name: productsArray[key].name,
+        price: productsArray[key].price,
+      },
+      qty: item.qty,
+    }));
+
+    const newOrder = new Order(req.body);
+    await newOrder.save();
+    res.json(newOrder);
+  },
+  updateOrder: async (req, res, next) => {
+    const orderUpdated = await Order.findByIdAndUpdate(
+      req.params.orderId,
+      req.body,
+      { new: true },
+    );
+    res.json(orderUpdated);
+  },
+  deleteOrder: async (req, res, next) => {
+    const deletedOrder = await Order.findByIdAndDelete(req.params.orderId);
+    res.json(deletedOrder);
+  },
+};

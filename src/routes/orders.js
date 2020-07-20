2ee2d3326ddc6873b9703d17b855de444434138e
Elena <karengvcs@gmail.com>
2020-07-19 23:21:25 -0500

    Solving Eslint errors

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,22 +1,22 @@
-const { requireAuth } = require("../middleware/auth");
+const { requireAuth } = require('../middleware/auth');
 const {
   getOrders,
   getOneOrder,
   createOrder,
   updateOrder,
   deleteOrder,
-} = require("../controller/orders.controller");
+} = require('../controller/orders.controller');
 
 module.exports = (app, nextMain) => {
-  app.get("/orders", requireAuth, getOrders);
+  app.get('/orders', requireAuth, getOrders);
 
-  app.get("/orders/:orderId", requireAuth, getOneOrder);
+  app.get('/orders/:orderId', requireAuth, getOneOrder);
 
-  app.post("/orders", requireAuth, createOrder);
+  app.post('/orders', requireAuth, createOrder);
 
-  app.put("/orders/:orderId", requireAuth, updateOrder);
+  app.put('/orders/:orderId', requireAuth, updateOrder);
 
-  app.delete("/orders/:orderId", requireAuth, deleteOrder);
+  app.delete('/orders/:orderId', requireAuth, deleteOrder);
 
   nextMain();
 };

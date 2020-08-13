5bbdfc28a021ff5c64081ff90671f367bc6e77ed
Elena <karengvcs@gmail.com>
2020-08-13 14:07:45 -0500

    updating files

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/burguer-queen';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://localhost:27017/burguer-queen-test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';

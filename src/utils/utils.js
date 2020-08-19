ebc481ebe96ec48fad1974fc5ca61778b6b32335
Elena <karengvcs@gmail.com>
2020-08-18 23:46:09 -0500

    Add funcions to valid email and password

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,3 +14,9 @@ module.exports.uidOrEmail = (parram) => {
   if (parram.indexOf('@') >= 0) return { email: parram };
   return { _id: parram };
 };
+
+module.exports.isAWeakPassword = (password) => ((password.length <= 3));
+module.exports.isAValidEmail = (email) => {
+  const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  return !!(emailRegex.test(email));
+};
